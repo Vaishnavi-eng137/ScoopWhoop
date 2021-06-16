4adbf59a027448b37d003044b6a95a3506e55a47
Vaishnavi-eng137 <vaisuch@gmail.com>
2021-06-16 20:44:01 +0530

    loader

--- src/pages/Storiespage.js
+++ src/pages/Storiespage.js
@@ -9,14 +9,22 @@ function Storiespage() {
   const [data, setData] = useState([]);
   const [displayButton, setDisplayButton] = useState(false);
   const [currentOffset, setCurrentOffset] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
   const pageRef = useRef(null);
 
   const getData = async () => {
-    const response = await fetch(
-      `https://www.scoopwhoop.com/api/v4/read/all/?offset=${currentOffset}&limit=12`
-    );
-    const dataList = await response.json();
-    setData([...data, ...dataList.data]);
+    try {
+      setIsLoading(true);
+      const response = await fetch(
+        `https://www.scoopwhoop.com/api/v4/read/all/?offset=${currentOffset}&limit=12`
+      );
+      const dataList = await response.json();
+      setData([...data, ...dataList.data]);
+      setIsLoading(false);
+    } catch (error) {
+      setData(data);
+      setIsLoading(false);
+    }
   };
 
   useEffect(() => {
@@ -30,13 +38,13 @@ function Storiespage() {
       onScroll={(e) => {
         const elem = e.target;
         if (elem.scrollHeight - elem.scrollTop - elem.clientHeight < 10) {
+          getData(currentOffset+12)
           setCurrentOffset((prev) => prev + 12);
         }
-        if(elem.scrollTop>30){
-          setDisplayButton(true)
-        }
-        else{
-          setDisplayButton(false)
+        if (elem.scrollTop > 30) {
+          setDisplayButton(true);
+        } else {
+          setDisplayButton(false);
         }
       }}
     >
@@ -47,6 +55,13 @@ function Storiespage() {
           return <Cards curElem={curElem} />;
         })}
       </div>
+      {isLoading && (
+        <div className="spin">
+        <div className="spinner-border spin" role="status">
+        </div>
+         <p>Loading...</p>
+         </div>
+      )}
       {displayButton && (
         <div
           className="scrollUp"
